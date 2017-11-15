926e6b0ec8c60408bb6a0bade849b35f3467c3cf
Josh Junon <junon@zeit.co>
2017-11-15 05:16:51 -0800

    make package detectable by require()

---
+++ null.js
@@ -0,0 +1,5 @@
+// Intentionally empty
+//
+// This file is required in order to make require.resolve() work,
+// which is a requirement of some helper scripts that check
+// for the existence of @zeit/git-hooks being installed.
