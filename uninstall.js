7a1c24350fe541d7bf174de1a4d5004237912980
Josh Junon <junon@wavetilt.com>
2021-01-29 18:02:20 +0100

    add better migration code for #7

--- uninstall.js
+++ uninstall.js
@@ -34,7 +34,7 @@ function uninstallHook(name) {
 		return;
 	}
 
-	const isOneOfOurs = fs.lstatSync(hookPath).isSymbolicLink() && fs.readlinkSync(hookPath) === './_do_hook.cjs';
+	const isOneOfOurs = fs.lstatSync(hookPath).isSymbolicLink() && fs.readlinkSync(hookPath).match(/\.\/_do_hook(\.cjs)?/);
 
 	if (!isOneOfOurs) {
 		console.error(`△  @zeit/git-hooks: hook '${name}' appears to be a user hook; skipping: ${hookPath}`);
@@ -75,5 +75,11 @@ function removeIfExists(path) {
 
 removeIfExists(path.join(hooksDir, '_do_hook.cjs'));
 removeIfExists(path.join(hooksDir, '_detect_package_hooks.cjs'));
+// XXX The following are legacy paths that should also be removed
+// XXX if they are found. This will help migration to the new commonjs
+// XXX extenions. See https://github.com/vercel/git-hooks/pull/7.
+// XXX These checks will most likely be removed at a later date.
+removeIfExists(path.join(hooksDir, '_do_hook'));
+removeIfExists(path.join(hooksDir, '_detect_package_hooks'));
 
 console.error('△  @zeit/git-hooks: hooks uninstalled successfully');
